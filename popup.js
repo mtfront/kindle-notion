a429c25e7c72909fdbcc1dc644508d87ce86544d
Mt Front <mfcndw@gmail.com>
2021-04-07 16:04:47 -0700

    Export and download csv, missing escaping

--- popup.js
+++ popup.js
@@ -9,7 +9,7 @@ chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
 
 button.addEventListener("click", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    buttonAction = inNotes(tab.url) ? exportNotes 
+    let buttonAction = inNotes(tab.url) ? exportNotes 
                                     :chrome.tabs.create({url: "https://read.amazon.com/notebook"});
 
     chrome.scripting.executeScript({
@@ -24,14 +24,30 @@ function inNotes(url) {
 
 function exportNotes(){
     // highlights = document.getElementsByClassName("kp-notebook-highlight");
-    rows = document.getElementsByClassName("kp-notebook-row-separator");
+    let rows = document.getElementsByClassName("kp-notebook-row-separator");
+    let notes = [["location", "highlight", "note"]];
     for (row of rows){
-        header = row.getElementsByClassName("kp-notebook-metadata");
+        let header = row.getElementsByClassName("kp-notebook-metadata");
         // a highlight row will have 2 metadata, e.g. Blue highlight | Page:1 & Note | Page: 1
         if (header.length < 2) {
             continue;
         }
-        separator = header[0].innerHTML.indexOf('|');
-        page = header[0].innerHTML.substring(separator + 2).replace('&nbsp;', ' ');
+        let separator = header[0].innerHTML.indexOf('|');
+        let page = header[0].innerHTML.substring(separator + 2).replace('&nbsp;', ' ');
+
+        // need to escape comma in the content with "", then escape quotes inside it.
+        let highlight = row.getElementsByClassName("a-color-base");
+        let content = highlight[0].innerHTML;
+        let note = highlight[1].innerHTML;
+        console.log(page + ',' + content + ',' + note);
+        notes.push([page, content, note]);
     }
+    // let title = document.getElementsByClassName("kp-notebook-annotation-container")[0].getElementsByClassName("kp-notebook-metadata")[1].innerHTML;
+    // let csv = notes.map(i => i.join(",")).join("\n");
+    // let csvFile = new Blob([csv], {type: "text/csv"});
+    // let link = document.createElement("a");
+    // link.download = title + ".csv";
+    // link.href = window.URL.createObjectURL(csvFile);
+    // document.body.appendChild(link);
+    // link.click();
 }
