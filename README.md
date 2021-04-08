2e2e64496092236e1da88ed2d1726eb8a4c86c3e
Mt Front <mfcndw@gmail.com>
2021-04-08 00:42:33 -0700

    Final styles, links and logo

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # kindle-notion
 A chrome plugin that export kindle notes to a notion compatible csv file.
+
+This plugin will convert the current selected kindle notes to a notion compatible csv file.
+
+Made for [my reading notes channel](https://t.me/mtfront) because filtering and sorting is just much more easier this way.
+
+## Output table format in Notion:
+
+| Name | Highlight | Note | No. |
+| --- | --- | --- | --- |
+|Chapter|Underlined highlights|Notes/thoughts|Index used to sort|
+
+
+## How to use:
+0. Install this chrome extension.
+1. Click the extension icon in chrome.
+2. The pop up will prompt you to kindle note page if you're not already in.
+3. Select the note you want to export and click an export. Browser will start to download the csv.
+5. In Notion, create a table (can be either inline or entire page)
+6. Click the '...' on right top cornor of your table, chose "Merge with CSV"
+7. Wait for import to complete and refresh the notion page
+8. Click the '...'->sort, add an ascending sort on 'No.' (why does stupid notion import in random order?)
+9. Notion may convert your 'Note' column to 'Type'. Simply click on the header and change property type to 'Text'
+10. Remove default unsed fields like "tags"
--- popup.js
+++ popup.js
@@ -25,29 +25,36 @@ function inNotes(url) {
 function exportNotes(){
     // highlights = document.getElementsByClassName("kp-notebook-highlight");
     let rows = document.getElementsByClassName("kp-notebook-row-separator");
-    let notes = [["location", "highlight", "note"]];
-    for (row of rows){
+    let notes = [["Location", "Highlight", "Note", "No."]];
+
+
+    function escapeCsv(s) {
+        return '"' + s.replace('"', '""') + '"';
+    }
+
+    // stupid notion will not sort csv, so we need the index to sort
+    let i = 1;
+    for (row of rows) {
         let header = row.getElementsByClassName("kp-notebook-metadata");
         // a highlight row will have 2 metadata, e.g. Blue highlight | Page:1 & Note | Page: 1
         if (header.length < 2) {
             continue;
         }
         let separator = header[0].innerHTML.indexOf('|');
-        let page = header[0].innerHTML.substring(separator + 2).replace('&nbsp;', ' ');
+        let page = escapeCsv(header[0].innerHTML.substring(separator + 2).replace('&nbsp;', ' '));
 
         // need to escape comma in the content with "", then escape quotes inside it.
         let highlight = row.getElementsByClassName("a-color-base");
-        let content = highlight[0].innerHTML;
-        let note = highlight[1].innerHTML;
-        console.log(page + ',' + content + ',' + note);
-        notes.push([page, content, note]);
-    }
-    // let title = document.getElementsByClassName("kp-notebook-annotation-container")[0].getElementsByClassName("kp-notebook-metadata")[1].innerHTML;
-    // let csv = notes.map(i => i.join(",")).join("\n");
-    // let csvFile = new Blob([csv], {type: "text/csv"});
-    // let link = document.createElement("a");
-    // link.download = title + ".csv";
-    // link.href = window.URL.createObjectURL(csvFile);
-    // document.body.appendChild(link);
-    // link.click();
+        let content = escapeCsv(highlight[0].innerHTML);
+        let note = escapeCsv(highlight[1].innerHTML);
+        notes.push([page, content, note, i++]);
+    };
+    let title = document.getElementsByClassName("kp-notebook-annotation-container")[0].getElementsByClassName("kp-notebook-metadata")[1].innerHTML;
+    let csv = notes.map(i => i.join(",")).join("\n");
+    let csvFile = new Blob([csv], {type: "text/csv"});
+    let link = document.createElement("a");
+    link.download = title + ".csv";
+    link.href = window.URL.createObjectURL(csvFile);
+    document.body.appendChild(link);
+    link.click();
 }
